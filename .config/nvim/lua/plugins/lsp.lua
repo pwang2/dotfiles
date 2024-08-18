3d3a8b2cea0aa3af5171ce78614812c8c937bf84
Peng Wang <gnaw.gnep@gmail.com>
2024-08-18 17:04:46 -0500

    clean

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,7 +13,7 @@ return {
 			require("lspconfig.ui.windows").default_options.border = borderStyle
 
 			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-			-- @diagnostic disable-next-line: duplicate-set-field
+			---@diagnostic disable-next-line: duplicate-set-field
 			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 				opts = opts or {}
 				opts.border = borderStyle
