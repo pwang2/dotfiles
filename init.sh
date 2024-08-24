ee9629ff91f1ba1e94a2d2380bb8d6324e4680bb
Peng Wang <gnaw.gnep@gmail.com>
2024-08-24 02:16:23 -0500

    fix

--- init.sh
+++ init.sh
@@ -20,6 +20,6 @@ ZPLUGINS=$HOME/.oh-my-zsh/custom/plugins
 [ ! -d $ZPLUGINS/zsh-autosuggestions ] && git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZPLUGINS/zsh-autosuggestions
 [ ! -d $ZPLUGINS/zsh-syntax-highlighting ] && git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZPLUGINS/zsh-syntax-highlighting
 [ ! -d $ZPLUGINS/zsh-vi-mode ] && git clone --depth=1 https://github.com/jeffreytse/zsh-vi-mode $ZPLUGINS/zsh-vi-mode
-[ ! -d $ZPLUGINS/zsh-evalcache ] && git clone --depth=1 https://github.com/mroth/evalcache $ZPLUGINS/zsh-evalcache
+[ ! -d $ZPLUGINS/evalcache ] && git clone --depth=1 https://github.com/mroth/evalcache $ZPLUGINS/evalcache
 
 [ ! -f $HOME/.zshrc ] && echo "source $HOME/.zshrc_shared" >$HOME/.zshrc
