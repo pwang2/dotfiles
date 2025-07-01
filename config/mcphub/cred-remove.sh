8c4024a8eb76ab81d4bb16578cfc8e9f8a6de644
Peng Wang <pwang@williamblair.com>
2025-06-30 19:54:55 -0500

    --wip-- [skip ci]

--- config/mcphub/cred-remove.sh
+++ config/mcphub/cred-remove.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-
-jq '.mcpServers.brave-search.env.BRAVE_API_KEY = ""' \
+# this was setup in incrontab
+jq '.mcpServers["brave-search"].env.BRAVE_API_KEY = ""' \
   "$HOME/.config/mcphub/servers.json" >"$CWD/config/mcphub/servers-no-creds.json"
