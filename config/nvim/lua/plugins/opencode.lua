818a42a729d4e77491ca743f08f0cac7846ff059
Peng Wang <pwang@williamblair.com>
2026-01-13 08:52:59 -0600

    --wip-- [skip ci]

--- config/nvim/lua/plugins/opencode.lua
+++ config/nvim/lua/plugins/opencode.lua
@@ -10,6 +10,9 @@ return {
     ---@type opencode.Opts
     vim.g.opencode_opts = {
       -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      provider = {
+        enabled = "tmux",
+      },
     }
 
     -- Required for `opts.events.reload`.
