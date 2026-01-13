0a3aeec1121a3bb426ad0db3b45506a1e5e1e89c
Peng Wang <pwang@williamblair.com>
2026-01-13 08:55:38 -0600

    --wip-- [skip ci]

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -24,7 +24,7 @@ return {
       },
       actions = {
         open_file = {
-          quit_on_open = true,
+          quit_on_open = false,
           eject = true,
           resize_window = true,
           window_picker = {
